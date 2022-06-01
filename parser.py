c26fd7b71ea0a878a8ae087c0d9985d59fe6eff0
EN16153 <satang16153@gmail.com>
2022-05-31 19:01:40 -0500

    first commit

--- parser.py
+++ parser.py
@@ -10,7 +10,6 @@ from nltk.tokenize import TweetTokenizer
 from nltk.corpus import stopwords as sw
 from nltk import word_tokenize, pos_tag
 import nltk.data
-
 """import ssl
 
 ##this made nltk.pos_tag and word_tokenize work for me
@@ -27,16 +26,18 @@ nltk.download('punkt')"""
 transformations = ['VEGETARIAN', 'NONVEG', 'VEGAN', 'NONVEGAN', 'NEWSTYLE', 'DOUBLE', 'HALVE']
 
 class Step:
-  def __init__(self, number, method, time, ingredients=[], tools=[]):
+  def __init__(self, text, number, method, time, ingredients=[], tools=[]):
     self.number = number
+    self.text = text    #
     self.method = method #cooking method
-    self.time = time
+    self.time = time    #
     self.ingredients=ingredients
-    self.tools=tools
+    self.tools=tools    #
 
 class Ingredient:
-    def __init__(self, name, quantity, unit, descriptors=[], prep=[]):
+    def __init__(self, text, name, quantity, unit, descriptors=[], prep=[]):
         self.name=name
+        self.text = text
         self.quantity=quantity
         self.unit=unit #of measurement
         self.descriptors=descriptors
@@ -66,8 +67,8 @@ def fetch_recipe(link):
     for x in stepsRaw[0]: 
         steps.append(x.contents[0])
 
-    for x in range(0,len(steps)):
-        print("Step", x+1, ":", steps[x])
+    '''for x in range(0,len(steps)):
+        print("Step", x+1, ":", steps[x])'''
 
 
     data = {"ingredients": ingredients, "steps": steps}
@@ -81,15 +82,55 @@ def parse_data():
     """
     return recipe
 
+Toolist = ['plate', 'bowl', 'microwave', 'pan', 'whisk', 'saucepan', 'pot', 'spoon', 'knive',
+        'oven', 'refrigerator', 'paper towels', 'baking dish', 'bag', 'tablespoon', 'teaspoon', 
+          'plates', 'bowls', 'whisks', 'saucepans', 'pots', 'spoons', 'knives',
+         'baking dishes', 'bags', 'tablespoons', 'teaspoons']
+Timelist = ['second', 'seconds', 'minute', 'minutes', 'hour', 'hours', 'day', 'days']
+
+
+def createStep(steps):
+    st = 0
+    for i in steps:
+        st += 1
+        tools = []
+        a = str(i.lower())
+        for item in Toolist:
+            if item in a and item not in tools:
+                tools.append(item)
+        y = nltk.word_tokenize(str(i.lower()))
+        print(i)
+        y = nltk.pos_tag(y)
+        
+        time = 'None'
+        #print(y)
+        for i in range(len(y)):
+            if y[i][0] in Timelist:
+                pt = -1
+                for index in range(i, i - 6, -1):
+                    if index < 0:   break
+                    if y[index][1] == 'CD': pt = index
+                if pt != -1:
+                    ans = y[pt][0]
+                    for j in range(pt + 1, i + 1):
+                        ans = ans + " " + y[j][0]
+                    time = ans
+
+        print("step", st, "tools =", tools)
+        print("step", st, "time =", time)
+        print("--------------------------------")
+    
 def main():
     # Your Code here
     print("Welcome to the Interactive Recipe Parser!")
 
-    url = 'https://www.allrecipes.com/recipe/244716/shirataki-meatless-meat-pad-thai/' 
+    url = 'https://www.allrecipes.com/recipe/16167/beef-bourguignon-i/' 
     #takes user input from command line
     #url = input("Please paste the url of the recipe you want to use:")
 
     rawData = fetch_recipe(url)
+
+    createStep(rawData["steps"])
 
     return
 
